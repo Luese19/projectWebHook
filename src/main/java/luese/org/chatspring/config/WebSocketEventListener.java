67718b31d88febd6c59f09eacdf73fb9484a93cc
luese <lueseandrey@outlook.com>
2024-12-13 20:23:33 +0800

    my first commit

--- src/main/java/luese/org/chatspring/config/WebSocketEventListener.java
+++ src/main/java/luese/org/chatspring/config/WebSocketEventListener.java
@@ -18,7 +18,7 @@ public class WebSocketEventListener {
     private final SimpMessageSendingOperations messageTemplate;
 
     @EventListener
-    public void HandleWebSocketConnectListener(SessionDisconnectEvent event) {
+    public void handleWebSocketConnectListener(SessionDisconnectEvent event) {
        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
        String username = (String) headerAccessor.getSessionAttributes().get("username").toString();
         if(username != null) {
